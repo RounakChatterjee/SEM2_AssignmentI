5e575c7ec7cc3aa660e618553b09a2684522e56d
RounakChatterjee <56823119+RounakChatterjee@users.noreply.github.com>
2020-03-13 01:56:11 +0530

    Update Gauss_Elimination_program.py

--- Gauss_Elimination_program.py
+++ Gauss_Elimination_program.py
@@ -6,3 +6,10 @@ A = np.array([[1.0,0.67,0.33],[0.45,1.0,0.55],[0.67,0.33,1.0]],dtype = np.float6
 b = 2.0*np.ones(3,dtype = np.float64)
 sol = np.linalg.solve(A,b)
 print("The Solution Vector is = \n",sol)
+
+'''
+computing for the problem solved using the fact that we were a two decimal places computer, the error creeped in due to
+truncation and hnce the value of the second vector varied from the expected result.
+
+
+'''
